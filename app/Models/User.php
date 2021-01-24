6b1cceff3e9bc3b67c2210d41a5704aa80642959
Horacio <horaciolazo@gmail.com>
2021-01-24 19:38:59 +0100

    modelos y relaciones

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    // relacion de uno a muchos
+    public function posts(){
+       return $this->hasMany('Post::class');
+    }
 }
